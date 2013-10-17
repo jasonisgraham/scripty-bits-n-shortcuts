879f18aa64973d00bfc7327f4c46cef3ce41a076
warhammer <jgraham84@gmail.com>
2013-10-16 20:06:23 -0400

    adding some stuff to recursively search files

--- bash/bash_customizations.sh
+++ bash/bash_customizations.sh
@@ -18,10 +18,15 @@ bind '"\ej"':"\"\eb\"";                 # alt-j moves cursor back word
 bind '"\ek"':"\"\ef\""                  # alt-k moves cursor forward word
 bind '"\eJ"':"\"\C-w\"";                # alt-J cuts word behind cursor
 bind '"\eK"':"\"\ed\""                  # alt-K cuts word in front of cursor
-bind '"\e<"':"\"\C-ubind-U\C-m\""       # alt-U Move up the directory. same as cd ../
-bind '"\e>"':"\"\C-ubind-F\C-m\""       # alt-f Move down the directory.
-bind '"\eL"':"\"\C-uls-type\C-m\""      # alt-L same as ls
+bind '"\e<"':"\"\C-u__move-up-directory\C-m\""       # alt-U Move up the directory. same as cd ../
+bind '"\e>"':"\"\C-u__move-down-directory\C-m\""       # alt-f Move down the directory.
+bind '"\eL"':"\"\C-u__ls-type\C-m\""      # alt-L same as ls
 bind '"\ew"':kill-region
+
+# grep -opts $search . searches recursively from PWD whatever string is on the prompt.  So, /some/dir> jason, then the binding, will execute "grep -rn jason ."  Doesnt work if you use single quotes
+bind '"\eXf"':"\"\C-a__find-in-files \C-e . rn \C-j\""      # case sensitive
+bind '"\eXi"':"\"\C-a__find-in-files \C-e . rni \C-j\""     # case insensitive
+bind '"\eXr"':"\"\C-a__find-in-files \C-e . rniP \C-j\""    # case insensitive, Uses Perl regex.  Encase this with some quotes
 
 # aliases
 alias ll='ls -AlF'
@@ -45,29 +50,31 @@ fi
 logitech=$(xinput --list --short | grep -m1 "Logitech" | cut -f2 | cut -d= -f2) # mouse ID
 xinput --set-prop "$logitech" "Device Accel Constant Deceleration" 2 # It defaults to 1
 
-##########################
+##################################
 #HISTORY_CUSTOMIZATIONS
-##########################
+##################################
 HISTCONTROL=ignoredups:ignorespace # don't put duplicate lines in the history.
 shopt -s histappend # append to the history file, don't overwrite it
 HISTSIZE=1000 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 HISTFILESIZE=2000
 
-##################################
-# Common useful functions
-##################################
-function bind-U {
+####################################################################
+# "useful" functions that are called with the binds from above
+# consider these private
+####################################################################
+function __move-up-directory {
     pushd . >> /dev/null
     cd ../
 }
 
-function bind-F {
+function __move-down-directory {
     popd >> /dev/null
 }
 
-function ls-type {
+function __ls-type {
+    local _type
     read -n 1 -s _type
-    ls_cmd='ls -glhBAF --ignore=#* --ignore=.svn --color=always'
+    ls_cmd='ls -glhBAF --ignore=#* --ignore=.git --color=always'
     case $_type in
         "h" )
         ;;
@@ -84,5 +91,41 @@ function ls-type {
     $ls_cmd
 }
 
-HAVE_SOURCED_GENERAL_BASH_CUSTOMIZATIONS=1
+function __find-in-files {
+    usage=$(cat <<FIND_IN_FILES_USAGE
+__find-in-files [needle] [grep_arguments] [haystack_dir] 
+FIND_IN_FILES_USAGE
+)
+
+    local needle=${1}
+    local haystack_dir=${2}
+    local grep_arguments=${3}
+
+    if [ -z ${needle} ]; then
+	echo "needle is not populated"
+	echo -e $usage
+	return 1;
+    fi
+    if [ -n "${4}" ]; then
+	echo "there is an extra parameter at position 4: '${4}'"
+	echo -e $usage
+	return 1;
+    fi
+    if [ -z ${haystack_dir} ]; then
+	haystack_dir="."
+    else
+	if ! [ -e ${haystack_dir} ]; then
+	    echo -e "haystack_dir: '${haystack_dir}' DNE"
+	    echo -e $usage
+	    return 1
+	fi
+    fi
+    if [ -z ${grep_arguments} ]; then
+	grep_arguments=rni
+    fi
+
+    local command="grep -${grep_arguments} --color ${needle} ${haystack_dir}"
+    echo $command
+    $command
+}
 
