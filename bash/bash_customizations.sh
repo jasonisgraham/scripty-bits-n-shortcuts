dee018b09b2380ad243917d3bdbf94b16e31477e
JASON <jgraham84@gmail.com>
2014-04-15 21:14:15 -0400

    Been a while sense i committed stuff I've been using

--- bash/bash_customizations.sh
+++ bash/bash_customizations.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# cp target/common-business-1.0.0.jar ../../../ui/thirdparty/web/target/ui-thirdparty/WEB-INF/lib/.
+# ./source/ui/admin/business/target/classes/other-sql.xml
+# find . -regextype sed -iregex ".*/[a-z\-]*sql\.xml"
+
 # BASH_FILES_DIR will be equal to the dir holding this file
 BASH_FILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -39,8 +43,15 @@ bind '"\eXr"':"\"\C-a__find-in-files \C-e . rniP \C-j\""
 # open file specified by whatever is currently printed to line in emacs in cygwin. Should be a file path (DOS or unix)
 # do not quote the argument!
 bind '"\eXe"':'"\C-aemacs $(convert-dos-filepath-to-cygwin-filepath "\C-e")\C-j\"'
+# copy what's on terminal line to clipboard
+bind '"\eX\ew"':'"\C-a echo \"\C-e\" | xclip -selection clipboard\C-j\"'
+# copy whatever ctrl+y does into clipboard
+bind '"\eX\ey"':'"echo \C-y | xclip -selection clipboard\C-j"'
+# inserts $() and moves cursor inside parens
+bind '"\eX("':'"$()\C-b"'
 
 # aliases
+alias to-clipboard='xclip -selection clipboard'
 alias ll='ls -AlF'
 alias la='ls -A'
 alias l='ls -CF'
@@ -55,7 +66,12 @@ alias h='history | tail'
 
 # sources
 if [ -a $BASH_FILES_DIR/bash_customizations_git.sh ]; then
-  source $BASH_FILES_DIR/bash_customizations_git.sh
+    source $BASH_FILES_DIR/bash_customizations_git.sh
+fi
+
+if [ -a $BASH_FILES_DIR/bash_customizations_svn.sh ]; then
+    source $BASH_FILES_DIR/bash_customizations_svn.sh
+    bind '"\e&"':"\"\C-udo-svn\C-m\""
 fi
 
 # THUMB BALL MOUSE ES 2 FASTO! SLOW IT DOWN
@@ -80,36 +96,118 @@ function convert-dos-filepath-to-cygwin-filepath {
     echo ${_CYGWIN_FILEPATH}
 }
 function __move-up-directory {
-  pushd . >> /dev/null
-  cd ../
+    pushd . >> /dev/null
+    cd ../
 }
 
 function __move-down-directory {
-  popd >> /dev/null
+    popd >> /dev/null
 }
 
 function __ls-type {
-  local _type
-  read -n 1 -s _type
-  ls_cmd='ls -glhBAF --ignore=#* --ignore=.git --color=always'
-  case $_type in
-    "h")
-    ;;
-    "t") # sort by modification time
-      ls_cmd="$ls_cmd -t"
-    ;;
-    "s") # sort by file size
-      ls_cmd="$ls_cmd -S"
-    ;;
-    *)
-      ls_cmd="ls --color=always"
-    ;;
-  esac
-  $ls_cmd
+    local _type
+    read -n 1 -s _type
+    ls_cmd='ls -glhBAF --ignore=#* --ignore=.git --color=always'
+    case $_type in
+	"h")
+	    ;;
+	"t") # sort by modification time
+	    ls_cmd="$ls_cmd -t"
+	    ;;
+	"s") # sort by file size
+	    ls_cmd="$ls_cmd -S"
+	    ;;
+	*)
+	    ls_cmd="ls --color=always"
+	    ;;
+    esac
+    $ls_cmd
+}
+
+function open-resource {
+    local resources=$(find-resource $1 $2)
+    if [[ $(echo $resources | wc -w) -ge 2 ]]; then
+	count=0;
+	for r in $resources
+	do
+	    r_arr[$count]=$r
+	    count=$(($count+1))
+	done
+	for key in ${!r_arr[@]}
+	do
+	    echo -n "$key "
+	    echo ${r_arr[$key]}
+	done
+	echo -e "\nSelect files to open by number(s).  If multiple, separate by space. If all, enter nothing or enter *.\n Which files(s): "
+	read _file_numbers
+	
+	local args_line=""
+	if [[ -z "$_file_numbers" ]]; then
+	    _file_numbers="*"
+	fi
+	if [[ "$_file_numbers" = "*" ]]; then
+	    args_line=$resources
+	else	
+	    for _file_number in $_file_numbers
+	    do
+		args_line="${args_line} ${r_arr[$_file_number]}"
+	    done
+	fi
+    else	
+	args_line=$resources
+    fi	
+    $EDITOR $args_line
+}
+
+function find-resource {
+    local _file_search_string=$(_wildcard-camelcase-file-search-string $1)
+    local _search_result_filter_string=$(_wildcard-camelcase-file-search-result-filter-string $1)   
+    local _haystack_dir=${2:-"."}    
+    find . -name $_file_search_string | grep -v target | grep -v bin | grep -v \~ | grep -P $_search_result_filter_string
+}
+
+function _wildcard-camelcase-file-search-string {
+   echo $(echo $1 | sed -E 's/([A-Z])/*\1/g' | sed 's/^\**//g' | sed 's/\*{2}/*/g')*
+}
+
+function _wildcard-camelcase-file-search-result-filter-string {
+    echo $(_wildcard-camelcase-file-search-string $1 | sed 's/\*$//' | sed 's/\*/[^A-Z]*/g')
+}    
+
+function str-contains {
+    local _haystack=$1
+    local _needle=$2
+    if [[ $(echo $_haystack | grep -P $_needle) ]]; then
+	echo 1
+    fi	
+}
+function grep-includes {
+    local _args=$1
+    if [[ "$2" ]]; then
+	local _includes="$2"
+    else
+	local _includes=""
+    fi
+    if [[ $(str-contains $_args j) ]]; then
+	_includes="${_includes} --include=\"*.java\""
+    fi
+    if [[ $(str-contains $_args g) ]]; then
+	_includes="${_includes} --include=\"*.groovy\""
+    fi
+    if [[ $(str-contains $_args x) ]]; then
+	_includes="${_includes} --include=\"*.xml\""
+    fi
+    if [[ $(str-contains $_args s) ]]; then
+	_includes="${_includes} --include=\"*.sql\""
+    fi
+    if [[ $(str-contains $_args p) ]]; then
+	_includes="${_includes} --include=\"*.properties\""
+    fi
+    echo $_includes
 }
 
 function __find-in-files {
-  usage=$(cat << FIND_IN_FILES_USAGE
+    usage=$(cat << FIND_IN_FILES_USAGE
   __find-in-files [needle] [grep_arguments] [haystack_dir]
 FIND_IN_FILES_USAGE)
 
