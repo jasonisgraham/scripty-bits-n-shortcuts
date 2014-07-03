a28e6aee742f4b7ee5c2b25f4ead7a7da30e411a
JASON <jgraham84@gmail.com>
2014-07-02 22:01:11 -0400

    Updating some git stuff

--- bash/bash_customizations_git.sh
+++ bash/bash_customizations_git.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
+#################################################
+# Binds "do-git" to Alt+Shift+g then awaits 
+#  single keystroke for what to do next.
+#  See usage what does what
+#################################################
+bind '"\eG"':"\"\C-udo-git\C-m\""       
+
+do-git-usage()
+{
+    cat <<EOF    
+    OPTIONS:
+    d           git diff
+    c           git commit
+    <           git pull origin master
+    >           git push origin master
+    s           git status
+    h           help
+
+    REASON FOR EXISTING:
+    Useful for someone coming from a subversion background.
+    Attempts to hide all the power stuff that git offers when all you wanna do is diff, commit, status, ... etc like one would do when working with SVN
+EOF
+}
+
+#################################################
+# See do-git-usage
+#################################################
 function do-git {
-    local GIT_BIN=git
+    local GIT_BIN=$(which git)
 
     local dir='.'
     if [[ "$1" ]]; then
@@ -12,38 +39,23 @@ function do-git {
 
     local CMD=
     case $_type in
-        "d" ) CMD="diff" ;;
-        "c" ) CMD="commit" ;;
+        d ) CMD="diff" ;;
+        c ) CMD="commit" ;;
         "<" )
-            CMD="pull origin master"
-            dir=''
-            ;;
+          CMD="pull origin master"
+          dir=''
+          ;;
         ">" )
-            CMD="push origin master"
-            dir=''
+          CMD="push origin master"
+          dir=''
+          ;;
+        s ) CMD="status" ;;
+        h|* )
+            do-git-usage        
             ;;
-        "s" ) CMD="status" ;;
-        "h" )
-            do-git-usage
-            return 0
-            ;;
-        *)  do-git s
-            return 0
-            ;;
-    esac
-
-    
-    ${GIT_BIN} $CMD $dir
+    esac    
+    local git_cmd="$GIT_BIN $CMD $dir"
+    echo ""
+    echo $git_cmd
+    $git_cmd
 }
-
-function do-git-usage {
-    echo "d           git diff"
-    echo "c           git commit"
-    echo "<           git pull origin master"
-    echo ">           git push origin master"
-    echo "s           git status"
-    echo "h           help"
-    echo "*           git status"
-}
-
-bind '"\eG"':"\"\C-udo-git\C-m\""       # start do-git
